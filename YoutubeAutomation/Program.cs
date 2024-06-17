4c62e3ccc9af2835fd8fe7a2dbba984cef517d9c
h <husnainm3@gmail.com>
2024-06-17 12:02:32 +0100

    Add project files.

---
+++ YoutubeAutomation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoutubeAutomation
+{
+    public class Program : YoutubeManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ YoutubeAutomation/YoutubeManager.cs
@@ -0,0 +1,22 @@
+﻿namespace YoutubeAutomation
+{
+    using Google.Apis;
+    using Google.Apis.Auth.OAuth2;
+
+    public class YoutubeManager
+    {
+        public HttpClient ApiHttpClient { get; set; }
+
+        public YoutubeManager()
+        {
+            ApiHttpClient = new HttpClient();
+            //ApiHttpClient.BaseAddress = 
+        }
+
+        private void Test()
+        {
+            var test = new GoogleWebAuthorizationBroker();
+            test.
+        }
+    }
+}
