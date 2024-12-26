9c5e607c12a4b8798e6cac6ed5646869e3513320
h <husnainm3@gmail.com>
2024-12-26 23:22:57 +0000

    added model

---
+++ RaindropAutomations/Models/Saving/MultiBookmarkModifyModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RaindropAutomations.Models.Saving
+{
+    public class MultiBookmarkModifyModel
+    {
+        [JsonPropertyName("ids")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public List<long> Ids { get; set; }
+
+        [JsonPropertyName("collection")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public CollectionIdSaveModel Collection { get; set; }
+
+        [JsonPropertyName("cover")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public string CoverUrl { get; set; }
+
+        [JsonPropertyName("tags")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public List<string> Tags { get; set; }  // DO NOT INTIALIZE AS EMPTY ARRAY WILL WIPE EXISTING DATA
+
+        [JsonPropertyName("important")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public bool? Important { get; set; }
+
+        [JsonPropertyName("media")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public List<Object> Media { get; set; }  // DO NOT INTIALIZE AS EMPTY ARRAY WILL WIPE EXISTING DATA
+    }
+
+}
