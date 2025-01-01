6d05af5880c22392e4454a7fcd8b42eeb55d4e51
h <husnainm3@gmail.com>
2025-01-01 17:33:19 +0000

    update model I missed

--- RaindropAutomations/Models/Saving/MultiBookmarkSavePayload.cs
+++ RaindropAutomations/Models/Saving/MultiBookmarkSavePayload.cs
@@ -1,19 +1,13 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace RaindropAutomations.Models.Saving
 {
     public class MultiBookmarkSavePayload
     {
-        [JsonPropertyName("result")]
+        [JsonProperty("result")]
         public bool Result { get; set; }
 
-        [JsonPropertyName("items")]
+        [JsonProperty("items")]
         public List<BookmarkSaveModel> Bookmarks { get; set; }
     }
 }
