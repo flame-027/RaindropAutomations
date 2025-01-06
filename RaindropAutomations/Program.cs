da5bbae388f1f9437d9ab6382455ea32c8d96152
h <husnainm3@gmail.com>
2025-01-06 21:57:43 +0000

    added DI container

--- RaindropAutomations/Program.cs
+++ RaindropAutomations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using RaindropAutomations.Models.Saving;
 using RaindropAutomations.Options;
 using RaindropAutomations.Services;
@@ -13,24 +15,35 @@ namespace RaindropAutomations
     {
         static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                         //.AddJsonFile("appsettings.json")
-                        .AddUserSecrets<Program>()
-                        .Build();
+            var host = Host.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                   //config.AddJsonFile("appsettings.json");
+                   config.AddUserSecrets<Program>();
+               })
+              .ConfigureServices(services =>
+              {
+                  services.AddSingleton<RaindropApiWrapService>();
+                  services.AddSingleton<RaindropOperationsService>();
+                  services.AddSingleton<RaindropRoutinesManager>();
+                  //services.AddSingleton<IMyService, MyService>();
+                  //services.AddTransient<IMyLogic, MyLogic>();
+              }).Build();
 
-            GlobalConfig.Config = config;
 
-            var saveCollectionId = int.Parse(config.GetFromRaindropConfig("VideosSaveInboxId").Value);
-            var checkParentCollectionId = int.Parse(config.GetFromRaindropConfig("VidoesCheckRootId").Value);
+            var config = host.Services.GetRequiredService<IConfiguration>();
+
+            var saveCollectionId = int.Parse(config["RaindropConfig:VideosSaveInboxId"]);
+            var checkParentCollectionId = int.Parse(config["RaindropConfig:VidoesCheckRootId"]);
 
             var playlistUrl = @"https://www.youtube.com/playlist?list=WL";
 
-            var routineManager = new RaindropRoutinesManager(new YoutubeManager(config));
+            var routineManager = host.Services.GetRequiredService<RaindropRoutinesManager>();
 
             routineManager.YoutubePlaylistToRaindrop(playlistUrl, saveCollectionId, checkParentCollectionId);
 
-            //raindropApiService.UpdateMultipleBookmarks(50386169, 50386170, [918116881]);    
+            //RaindropApiWrapService.UpdateMultipleBookmarks(50386169, 50386170, [918116881]);    
         }
 
 
