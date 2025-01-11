cdfb3376b3282d09500ddc7e0ddb55ad8c260850
h <husnainm3@gmail.com>
2025-01-11 13:06:15 +0000

    fixed GetUrlType extention method to work with new use case

--- RaindropAutomations/tools/ExtensionMethods.cs
+++ RaindropAutomations/tools/ExtensionMethods.cs
@@ -22,12 +22,15 @@ namespace RaindropAutomations.Tools
             return sectionResult;
         }
 
-        public static string GetUrlType(this string input, UrlOptions urlType)
+        public static string GetUrlType(this string input, UrlOptions? urlType)
         {
             if (string.IsNullOrEmpty(input))
                 return string.Empty;
 
-            if(urlType == UrlOptions.PureUrl)
+            if(urlType == null)
+                return string.Empty;
+
+            if (urlType == UrlOptions.PureUrl)
             {
                 int index = input.IndexOf('?');
                 return index >= 0 ? input.Substring(0, index).Trim() : input.Trim();
