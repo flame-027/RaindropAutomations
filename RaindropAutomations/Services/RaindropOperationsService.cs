532a38ec9d3f2b39fb6eddd75fcab8044fb6b662
h <husnainm3@gmail.com>
2025-01-11 12:56:16 +0000

    refactored & cleaned up RemoveMatchesFromDescendants

--- RaindropAutomations/Services/RaindropOperationsService.cs
+++ RaindropAutomations/Services/RaindropOperationsService.cs
@@ -3,6 +3,7 @@ using RaindropAutomations.Models.Fetching;
 using RaindropAutomations.Models.Processing;
 using RaindropAutomations.Models.Saving;
 using RaindropAutomations.Tools;
+using System.Security.Cryptography.X509Certificates;
 
 namespace RaindropAutomations.Services
 {
@@ -117,49 +118,37 @@ namespace RaindropAutomations.Services
             return allBookmarks;
         }
 
-        public void RemoveMatchesFromDescendants(List<BookmarkSaveModel> inputList, long parentCollectionId, SelfInclusionOptions includeParentSettings, UrlOptions? MatchOptions = null)
+        public List<BookmarkSaveModel> RemoveMatchesFromDescendants(List<BookmarkSaveModel> inputList, long parentCollectionId, SelfInclusionOptions includeParentSettings, UrlOptions? matchOptions = null)
         {
             // FINDING MATCHES
             var allMatchingBookmarks = new List<BookmarkFetchModel>();
 
-            if (includeParentSettings == SelfInclusionOptions.ExcludeSelf)
-            {
-                var descendantCollections = GetDescendantCollectionsById(parentCollectionId);
-                var descendantBookmarks = GetAllBookmarksFromMultipleCollections(descendantCollections.AllIdsWithinForest);
+            var collections = includeParentSettings == SelfInclusionOptions.IncludeSelf
+                                                          ? GetDescendantAndSelfCollectionsById(parentCollectionId)
+                                                          : GetDescendantCollectionsById(parentCollectionId);
 
-                allMatchingBookmarks = descendantBookmarks ?? [];
-            }
+            allMatchingBookmarks = GetAllBookmarksFromMultipleCollections(collections.AllIdsWithinForest) ?? [];
 
-            if (includeParentSettings == SelfInclusionOptions.IncludeSelf)
-            {
-                var parentAndDescendantCollections = GetDescendantAndSelfCollectionsById(parentCollectionId);
-                var allBookmarksInParentAndDescendants = GetAllBookmarksFromMultipleCollections(parentAndDescendantCollections.AllIdsWithinForest);
+            if (allMatchingBookmarks.Count < 1)
+                return inputList;
 
-                allMatchingBookmarks = allBookmarksInParentAndDescendants ?? [];
-            }
-
-            // could stop repeated code here by having conditional however I currently like how easy this is to read
-            // with seperate concerns and specific variable names. May change down the line depending on hows this method evolves.
+            // GET BOOKMARK URLS AND REFINE IF NEED BE
+            var convertedBookmarkUrls = allMatchingBookmarks.Select(x => x.Link.GetUrlType(matchOptions));
 
             // REMOVING MATCHES
-            if (MatchOptions != null && MatchOptions is UrlOptions confirmedType)
-            {
-                var refinedMatchingUrls = allMatchingBookmarks.Select(x => x.Link
-                                                        .GetUrlType(confirmedType))
-                                                        .ToList();
 
-                inputList.RemoveAll(x => refinedMatchingUrls.Contains(x.Link
-                                                          .GetUrlType(confirmedType)));
-            }
-            else
-            {
-                var rawMatchingUrls = allMatchingBookmarks.Select(x => x.Link)
-                                                    .ToList();
+            inputList.RemoveAll(x => convertedBookmarkUrls.Contains(x.Link.GetUrlType(matchOptions)));
 
-                inputList.RemoveAll(x => rawMatchingUrls.Contains(x.Link));
-            }
-            // the else statment could potentially be removed, if support for none was added to options enum 
+            return inputList;
         }
+
+        private List<string> GetMatchingUrls(List<BookmarkFetchModel> bookmarks, UrlOptions? matchOptions)
+        {
+            return matchOptions != null
+                ? bookmarks.Select(x => x.Link.GetUrlType(matchOptions.Value)).ToList()
+                : bookmarks.Select(x => x.Link).ToList();
+        }
+
 
     }
 }
---
+++ RaindropAutomations/tools/ExtensionMethods.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using RaindropAutomations.Core.Options;
+using RaindropAutomations.Models.Fetching;
+using RaindropAutomations.Models.Saving;
+using RaindropAutomations.Services;
+
+namespace RaindropAutomations.Tools
+{
+    public static class ExtensionMethods
+    {
+        public static IConfigurationSection GetRaindropConfig(this IConfiguration config)
+        {
+            var raindropConfig = config.GetSection("Raindrop");
+
+            return raindropConfig;
+        }
+
+        public static IConfigurationSection GetFromRaindropConfig(this IConfiguration config, string sectionToGet) 
+        {
+            var sectionResult = config.GetRaindropConfig().GetSection(sectionToGet);
+
+            return sectionResult;
+        }
+
+        public static string GetUrlType(this string input, UrlOptions urlType)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            if(urlType == UrlOptions.PureUrl)
+            {
+                int index = input.IndexOf('?');
+                return index >= 0 ? input.Substring(0, index).Trim() : input.Trim();
+            }
+
+            if (urlType == UrlOptions.UrlAndFirstParamOnly)
+            {
+                int index = input.IndexOf('&');
+                return index >= 0 ? input.Substring(0, index).Trim() : input.Trim();
+            }
+
+            else
+                throw new NotImplementedException("Support has not been added for this url option");
+
+            //return input.Split('&')[0].Trim();
+            // easy to read but apparently has unnecessary overhead
+        }
+        
+
+    }
+}
